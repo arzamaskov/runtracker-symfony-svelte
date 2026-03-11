0510a74163fb75cac2c392d01c93146fd639796f
Andrey Arzamaskov <arzamaskov@gislavedtires.ru>
2026-03-11 14:39:49 +0400

    refactor: import InvalidArgumentException in Role enum

--- backend/src/Identity/Domain/Enum/Role.php
+++ backend/src/Identity/Domain/Enum/Role.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\Identity\Domain\Enum;
 
+use InvalidArgumentException;
+
 enum Role: string
 {
     case USER = 'ROLE_USER';
     case ADMIN = 'ROLE_ADMIN';
 
     /**
-     * @throws \InvalidArgumentException When role is неизвестна/неподдерживаемая.
+     * @throws InvalidArgumentException When role is неизвестна/неподдерживаемая.
      */
     public static function fromString(string $role): self
     {
@@ -18,7 +20,7 @@ enum Role: string
         $value = self::tryFrom($normalized);
 
         if ($value === null) {
-            throw new \InvalidArgumentException(sprintf('Unknown role "%s".', $role));
+            throw new InvalidArgumentException(sprintf('Unknown role "%s".', $role));
         }
 
         return $value;
